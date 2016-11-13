6c6e3babef93008b3e08b105de16a2d43bab3127
Hannah Meyers <hlmeyers@ucsc.edu>
2016-11-13 08:39:49 -0800

    Add files via upload

---
+++ DataParser.py
@@ -0,0 +1,32 @@
+#! usr/bin/env python3
+
+import sys
+
+def deletionDataParser(file = ''):
+    """Parser for handling deletion data. File to be extracted can be specified when function is called,
+    maybe we want to specify something in the command line, or it can be read from sys.stdin
+
+    Possible additions: determine whether heterozygous or homozygous and return that as well as the dictionary"""
+
+    if file is '':
+        f = sys.stdin
+    else:
+        f = open(file)
+
+    inputData = dict() #key = (knockout, gene), value = gene expression level | knockout
+    genes = list() #genes extracted from file
+
+    for i, line in enumerate(f):
+        if i == 0:
+            genes = line.strip().replace('"', '').split('\t')
+            genes.pop(0)
+        elif line.rstrip():
+            print(line)
+            geneLine = line.strip().split('\t')
+            thisKnockout = geneLine.pop(0).replace('"', '')
+            print(geneLine)
+            for i, gene in enumerate(genes):
+                inputData[(thisKnockout, gene)] = geneLine[i]
+
+    return inputData
+
