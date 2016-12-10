515b62acf26e1d2d2018c0dadb8ae5ed71e1cc5a
Hannah Meyers <hlmeyers@ucsc.edu>
2016-12-09 20:49:54 -0800

    Update DataParser.py
    Final update

--- DataParser.py
+++ DataParser.py
@@ -1,6 +1,12 @@
-#! usr/bin/env python3
+#! usr/bin/env python2.7
 
-import sys
+from __future__ import  print_function
+
+import sys, numpy
+from collections import defaultdict
+from decimal import *
+
+getcontext().prec = 28
 
 def deletionDataParser(file = ''):
     """Parser for handling deletion data. File to be extracted can be specified when function is called,
@@ -15,6 +21,9 @@ def deletionDataParser(file = ''):
 
     inputData = dict() #key = (knockout, gene), value = gene expression level | knockout
                     #or wt, gene for wild type strain
+    initial_wt = dict () #dictionary of wildtype expression levels
+    initial_gene_exp = defaultdict(lambda: defaultdict(dict)) #dictionary of gene expression levels not including wt
+
     genes = list() #genes extracted from file
 
     for i, line in enumerate(f):
@@ -25,11 +34,21 @@ def deletionDataParser(file = ''):
             #print(line)
             geneLine = line.strip().split('\t')
             thisKnockout = geneLine.pop(0).replace('"', '')
-            print(geneLine)
+            #print(geneLine)
             for i, gene in enumerate(genes):
                 if thisKnockout == 'wt':
-                    inputData[(thisKnockout, gene)] = geneLine[i]
+                    inputData[(thisKnockout, gene)] = float(geneLine[i])
                 else:
-                    inputData[(thisKnockout[:-5], gene)] = geneLine[i]
+                    inputData[(thisKnockout[:-5], gene)] = float(geneLine[i])
 
-    return inputData, genes
+    for strain, x in inputData.items():
+        knockout, gene = strain
+        if knockout == 'wt':
+            initial_wt[gene] = float(x)
+        else:
+            # gene = a, knockout = b
+            initial_gene_exp[gene][knockout] = float(x)
+
+    return inputData, initial_wt
+
+
